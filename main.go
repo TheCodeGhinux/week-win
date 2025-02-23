74fc518b1021a6a489402ab53185767627d4ef91
CodeGhinux <ilesanmibry@gmail.com>
2025-02-23 09:59:48 +0100

    feat: updated routers with base url endpoint

--- main.go
+++ main.go
@@ -1,20 +1,12 @@
 package main
 
 import (
-	"net/http"
-
 	"github.com/TheCodeGhinux/week-win/pkg/routing"
-	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	r := gin.Default()
-	r.GET("/ping", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"message": "pong",
-		})
-	})
+	// r := gin.Default()
 	routing.Route()
 
-	r.Run("0.0.0.0:8080")
+	// r.Run("0.0.0.0:8080")
 }
